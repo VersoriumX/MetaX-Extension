83161e48f23cf6facd1ee5eff9db628adf3997c6
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2024-11-24 09:37:59 -0500

    Update issue.ts
    Signed-off-by: Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>

--- .github/scripts/shared/issue.ts
+++ .github/scripts/shared/issue.ts
@@ -1,183 +1,54 @@
-import { GitHub } from '@actions/github/lib/utils';
-
-import { LabelableType, Labelable } from './labelable';
-
-// This function retrieves an issue on a specific repo
-export async function retrieveIssue(
-  octokit: InstanceType<typeof GitHub>,
-  repoOwner: string,
-  repoName: string,
-  issueNumber: number,
-): Promise<Labelable> {
-  const retrieveIssueQuery = `
-      query GetIssue($repoOwner: String!, $repoName: String!, $issueNumber: Int!) {
-        repository(owner: $repoOwner, name: $repoName) {
-          issue(number: $issueNumber) {
-            id
-            createdAt
-            body
-            author {
-              login
-            }
-            labels(first: 100) {
-              nodes {
-                id
-                name
-              }
-            }
-          }
-        }
-      }
-    `;
-
-  const retrieveIssueResult: {
-    repository: {
-      issue: {
-        id: string;
-        createdAt: string;
-        body: string;
-        author: {
-          login: string;
-        };
-        labels: {
-          nodes: {
-            id: string;
-            name: string;
-          }[];
-        };
-      };
-    };
-  } = await octokit.graphql(retrieveIssueQuery, {
-    repoOwner,
-    repoName,
-    issueNumber,
-  });
-
-  const issue: Labelable = {
-    id: retrieveIssueResult?.repository?.issue?.id,
-    type: LabelableType.Issue,
-    number: issueNumber,
-    repoOwner: repoOwner,
-    repoName: repoName,
-    createdAt: retrieveIssueResult?.repository?.issue?.createdAt,
-    body: retrieveIssueResult?.repository?.issue?.body,
-    author: retrieveIssueResult?.repository?.issue?.author?.login,
-    labels: retrieveIssueResult?.repository?.issue?.labels?.nodes,
-  };
-
-  return issue;
-}
-
-// This function retrieves the list of linked issues for a pull request
-export async function retrieveLinkedIssues(
-  octokit: InstanceType<typeof GitHub>,
-  repoOwner: string,
-  repoName: string,
-  prNumber: number,
-): Promise<Labelable[]> {
-  // We assume there won't be more than 100 linked issues
-  const retrieveLinkedIssuesQuery = `
-  query ($repoOwner: String!, $repoName: String!, $prNumber: Int!) {
-    repository(owner: $repoOwner, name: $repoName) {
-      pullRequest(number: $prNumber) {
-        closingIssuesReferences(first: 100) {
-          nodes {
-            id
-            number
-            createdAt
-            body
-            author {
-                login
-            }
-            labels(first: 100) {
-                nodes {
-                    id
-                    name
-                }
-            }
-            repository {
-              name
-              owner {
-                login
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  `;
-
-  const retrieveLinkedIssuesResult: {
-    repository: {
-      pullRequest: {
-        closingIssuesReferences: {
-          nodes: Array<{
-            id: string;
-            number: number;
-            createdAt: string;
-            body: string;
-            author: {
-              login: string;
-            };
-            labels: {
-              nodes: {
-                id: string;
-                name: string;
-              }[];
-            };
-            repository: {
-              name: string;
-              owner: {
-                login: string;
-              };
-            };
-          }>;
-        };
-      };
-    };
-  } = await octokit.graphql(retrieveLinkedIssuesQuery, {
-    repoOwner,
-    repoName,
-    prNumber,
-  });
-
-  const linkedIssues: Labelable[] =
-    retrieveLinkedIssuesResult?.repository?.pullRequest?.closingIssuesReferences?.nodes?.map(
-      (issue: {
-        id: string;
-        number: number;
-        createdAt: string;
-        body: string;
-        author: {
-          login: string;
-        };
-        labels: {
-          nodes: {
-            id: string;
-            name: string;
-          }[];
-        };
-        repository: {
-          name: string;
-          owner: {
-            login: string;
-          };
-        };
-      }) => {
-        return {
-          id: issue?.id,
-          type: LabelableType.Issue,
-          number: issue?.number,
-          repoOwner: issue?.repository?.owner?.login,
-          repoName: issue?.repository?.name,
-          createdAt: issue?.createdAt,
-          body: issue?.body,
-          author: issue?.author?.login,
-          labels: issue?.labels?.nodes,
-        };
-      },
-    ) || [];
-
-  return linkedIssues;
-}
+/// <reference types="node" />
+/**
+ * Pads the front of the given hex string with zeroes until it reaches the
+ * target length. If the input string is already longer than or equal to the
+ * target length, it is returned unmodified.
+ *
+ * If the input string is "0x"-prefixed or not a hex string, an error will be
+ * thrown.
+ *
+ * @param hexString - The hexadecimal string to pad with zeroes.
+ * @param targetLength - The target length of the hexadecimal string.
+ * @returns The input string front-padded with zeroes, or the original string
+ * if it was already greater than or equal to to the target length.
+ */
+export declare function padWithZeroes(hexString: string, targetLength: number): string;
+/**
+ * Returns `true` if the given value is nullish.
+ *
+ * @param value - The value being checked.
+ * @returns Whether the value is nullish.
+ */
+export declare function isNullish(value: any): boolean;
+/**
+ * Convert a value to a Buffer. This function should be equivalent to the `toBuffer` function in
+ * `ethereumjs-util@5.2.1`.
+ *
+ * @param value - The value to convert to a Buffer.
+ * @returns The given value as a Buffer.
+ */
+export declare function legacyToBuffer(value: unknown): Buffer;
+/**
+ * Concatenate an extended ECDSA signature into a single '0x'-prefixed hex string.
+ *
+ * @param v - The 'v' portion of the signature.
+ * @param r - The 'r' portion of the signature.
+ * @param s - The 's' portion of the signature.
+ * @returns The concatenated ECDSA signature as a '0x'-prefixed string.
+ */
+export declare function concatSig(v: Buffer, r: Buffer, s: Buffer): string;
+/**
+ * Recover the public key from the given signature and message hash.
+ *
+ * @param messageHash - The hash of the signed message.
+ * @param signature - The signature.
+ * @returns The public key of the signer.
+ */
+export declare function recoverPublicKey(messageHash: Buffer, signature: string): Buffer;
+/**
+ * Normalize the input to a lower-cased '0x'-prefixed hex string.
+ *
+ * @param input - The value to normalize.
+ * @returns The normalized value.
+ */
+export declare function normalize(input: number | string): string;
